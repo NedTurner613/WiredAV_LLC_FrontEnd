0155f29edf9f88e45f90525bf6e07561d8a96efe
Kareem42 <kareem4209@gmail.com>
2026-08-04 18:29:23 -0500

    created project architecture with placeholder code to be deleted later.

---
+++ wire-av-app/src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+export default function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat quam
+        minus unde vel velit expedita eaque tempore labore recusandae ipsum
+        laboriosam dicta veritatis sed quas dolore, laborum fuga a optio!
+      </p>
+    </div>
+  );
+}
