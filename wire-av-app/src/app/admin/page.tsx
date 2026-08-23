12e7eede66f582e4f5578d53b9092117af279e45
ermonio <moni.osborne07@gmail.com>
2026-08-23 18:06:34 -0500

    BUG Fix for admin page and edited db mock data

--- wire-av-app/src/app/admin/page.tsx
+++ wire-av-app/src/app/admin/page.tsx
@@ -37,10 +37,10 @@ export default async function AdminPage(role: Role) {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
-              <div className="grid grid-cols-1 gap-6 @[1200px]:grid-cols-[0.5fr_1fr]">
-                {/* <DataTable data={data} /> */}
-                <div className="min-w-0 sticky top-6 max-h-[600px] overflow-y-auto">
+              <div className="rounded-2xl border border-slate-200/70 bg-linear-to-br from-slate-50 to-white p-5 shadow-sm">
+                <div className="flex flex-col gap-6">
+                  <SectionCards />
+                  {/* <DataTable data={data} /> */}
                   <CalendarPicker />
                 </div>
               </div>
--- wire-av-app/src/types/index.ts
+++ wire-av-app/src/types/index.ts
@@ -4,3 +4,33 @@ type User = {
   name: string;
   role: Role;
 };
+
+type Personnel = {
+id: number;
+firstName: string;
+lastName: string;
+email: string;
+role: "admin" | "technician";
+}
+
+type Client = {
+  id: string;
+  name: string;
+  email: string;
+  phone?: string;
+};
+
+type Appointment = {    
+id: string; 
+status: "requested" | "assigned" | "closed" | "canceled";
+personnel: Personnel[];
+client: Client[];
+timeslot: Timeslot[];
+appType: "consultation" | "installation" | "maintenance";
+}
+
+type Timeslot = { 
+    id: string;
+    startTime: string; // ISO 8601 format
+    endTime: string;   // ISO 8601 format
+}
