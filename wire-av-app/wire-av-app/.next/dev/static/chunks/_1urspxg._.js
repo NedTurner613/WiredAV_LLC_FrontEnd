f231537f6adf9c5e54301bcb29a8302091dd1cb4
Justin Alcendor <kareem4209@gmail.com>
2026-09-09 08:33:42 -0500

    Delete wire-av-app/wire-av-app/.next/dev/static directory

--- wire-av-app/wire-av-app/.next/dev/static/chunks/_1urspxg._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__044-sh7._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_09beqs_._.js","static/chunks/_0ugvtjh._.js"],
-    source: "entry"
-});
